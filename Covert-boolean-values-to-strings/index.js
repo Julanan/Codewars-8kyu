0347dd5322d8ea4d62881acd7ad7d2e1fc318fe9
Komin J <102883710+Julanan@users.noreply.github.com>
2022-05-01 22:11:40 -0700

    8kyu

---
+++ Covert-boolean-values-to-strings/index.js
@@ -0,0 +1,8 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+//Given: a boolean value
+//Return: a 'Yes' string for true and a 'No' string for false
+
+function boolToWord( bool ){
+    if (bool) return 'Yes'
+      else return 'No' 
+  }
