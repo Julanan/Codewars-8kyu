c02ddf3417bb7f18194846a822d5801635b88e9c
Komin J <102883710+Julanan@users.noreply.github.com>
2022-05-01 22:11:07 -0700

    Delete Covert-boolean-values-to-strings directory

--- Covert-boolean-values-to-strings/index.js.js
+++
@@ -1,8 +0,0 @@
-//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-//Given: a boolean value
-//Return: a 'Yes' string for true and a 'No' string for false
-
-function boolToWord( bool ){
-    if (bool) return 'Yes'
-      else return 'No' 
-  }
