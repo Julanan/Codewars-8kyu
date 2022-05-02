41b2c4798208cc1d66f9127f93ee0144d4e0cbe4
Komin J <102883710+Julanan@users.noreply.github.com>
2022-05-02 13:26:14 -0700

    8kyu

---
+++ Validate-code-with-simple-regex/index.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/56a25ba95df27b7743000016/train/javascript
+
+// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+// You can assume the input will always be a number.
+
+// SAMPLE TEST
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(validateCode(123), true);
+//     assert.strictEqual(validateCode(248), true);
+//     assert.strictEqual(validateCode(8), false);
+//     assert.strictEqual(validateCode(321), true);
+//     assert.strictEqual(validateCode(9453), false);  })
+
+// })
+
+function validateCode (code) {
+    let newCode = /^[123]/g;
+    return newCode.test(code)
+  }
