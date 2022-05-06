347f708fcb2b117d94cc5c4c6bb5f3cde9c92c0f
Komin J <102883710+Julanan@users.noreply.github.com>
2022-05-06 08:41:57 -0700

    Training JS #4

---
+++ Basic-data-types-Array/index.js
@@ -0,0 +1,48 @@
+// https://www.codewars.com/kata/571effabb625ed9b0600107a/train/javascript
+
+// Task
+// I've written five function, each function receives a parameter: arr(an array), you should code something with arr.
+
+//     1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+// SAMPLE TextDecoderStreamdescribe("Tests", () => {
+//     it("test", () => {
+//       Test.assertSimilar(getLength([1,2,3]),3);
+//       Test.assertSimilar(getFirst([1,2,3]),1);
+//       Test.assertSimilar(getLast([1,2,3]),3);
+//       Test.assertSimilar(pushElement([1,2,3]).length,4);
+//       Test.assertSimilar(popElement([1,2,3]).length,2);
+//     });
+//   });
+  
+
+function getLength(arr){
+    //return length of arr
+    return ?
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return ?
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return ?
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    
+    return ?
+  }
