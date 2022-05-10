822a3f89f50c31a5c21295b92dd9d22ee7d87935
Komin J <102883710+Julanan@users.noreply.github.com>
2022-05-10 10:20:20 -0700

    8kyu

---
+++ Multiply/index.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
+// This code does not execute properly. Try to figure out why.
+
+// SAMPLE TEST 
+// const assert = require("chai").assert;
+
+// describe("Multiply", () => {
+//   it("fixed tests", () => {
+//     assert.strictEqual(multiply(1,1), 1);
+//     assert.strictEqual(multiply(2,1), 2);
+//     assert.strictEqual(multiply(2,2), 4);
+//     assert.strictEqual(multiply(3,5), 15);   
+//   });
+// });
+
+const multiply = (a, b) => a * b
+
+
+function multiply(a, b){
+    return a * b
+  }
